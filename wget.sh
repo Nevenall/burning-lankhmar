3ad5abd723ab6b06856f003158192a2569b190aa
Dan Behlings <nevenall@hotmail.com>
2020-12-10 19:50:48 -0800

    packages and wget

---
+++ wget.sh
@@ -0,0 +1,3 @@
+sudo mount -t cifs -o username=nevenall@hotmail.com //192.168.1.161/s/game-assets /mnt/game-assets
+
+sudo wget2 --recursive -l inf -t 5 --no-parent --ignore-case --accept jpg,jpeg,png,webp https://thetrove.is/Assets/By%20Artist%20or%20Source
